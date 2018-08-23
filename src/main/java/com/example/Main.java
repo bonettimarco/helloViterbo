26f8adc944db8a8a9fe02d34e6a6f74baf894a62
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-23 09:26:05 -0300

    Hello via JSP

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -56,7 +56,7 @@ public class Main {
 
   @RequestMapping("/HelloViterbo")
   public String getHelloViterbo() {
-    return "Get some Foos with Header";
+    return "HelloViterboView";
   }
 
   @RequestMapping("/db")

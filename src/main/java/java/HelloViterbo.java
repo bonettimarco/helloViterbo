01b95df500f8f9c28e2dbb7f03aa6170f8d0a69b
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-23 08:23:20 -0300

    Hello via JSP

--- src/main/java/java/HelloViterbo.java
+++ src/main/java/java/HelloViterbo.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  *
@@ -29,7 +28,7 @@ public class HelloViterbo extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 

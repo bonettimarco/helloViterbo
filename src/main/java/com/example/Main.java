1ef4ce6b81b30efc4ecdf119566742d2a9340128
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-23 09:59:37 -0300

    Hello via JSP

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -55,7 +55,7 @@ public class Main {
 
 
   @RequestMapping("/HelloViterbo")
-    return "HelloViterbo";
+     String helloViterbo() { return "HelloViterbo"; }
 
   @RequestMapping("/db")
   String db(Map<String, Object> model) {

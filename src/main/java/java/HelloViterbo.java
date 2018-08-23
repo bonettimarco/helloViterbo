a7f101a1da166c529d46a805a765b7837d3ab6ac
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-23 08:50:18 -0300

    Hello via JSP

--- src/main/java/java/HelloViterbo.java
+++ src/main/java/java/HelloViterbo.java
@@ -10,12 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
  * @author marco
  */
-@WebServlet(name = "HelloViterbo", urlPatterns = ("/HelloViterbo"))
+@WebServlet(name="HelloViterbo", urlPatterns = ("/HelloViterbo"))
 public class HelloViterbo extends HttpServlet {
 
     /**
@@ -27,13 +28,21 @@ public class HelloViterbo extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-
-    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-
-        javax.servlet.RequestDispatcher rd = request.getRequestDispatcher("HelloViterboView.jsp");
-        rd.forward(request, response);
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloViterbo</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello via Servlet</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">

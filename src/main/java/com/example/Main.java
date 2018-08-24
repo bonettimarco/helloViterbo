e296279f98da80175dd39153837274835e4305cf
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-24 20:02:30 +0000

    web inf

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -60,6 +60,11 @@ public class Main {
     return "index";
   }
 
+  @RequestMapping("/HelloViterbo")
+   String olah() {
+    return "HelloViterbo";
+  }
+
   @RequestMapping("/db")
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {

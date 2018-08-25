50edd08fa5665f1da8bf58f32aa8a0950ca72efc
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-25 11:25:39 +0000

    main alterado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -30,6 +30,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 //  import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web,bind.annotation.RequestParam;
 
 import javax.sql.DataSource;
 import java.sql.Connection;

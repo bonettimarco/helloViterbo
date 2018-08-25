a1ecb063b06b1d608a13011a6be925a751a9a8f2
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-25 10:33:41 +0000

    main alterado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -61,7 +61,7 @@ public class Main {
   }
 
   @RequestMapping("/HelloViterbo")
-  String nome = "Marco"
+  String nome = "Marco";
     String olah(Map<String, Object> model) {
       ArrayList<String> output = new ArrayList<String>();
       output.add("Hello World via Servlet"); 

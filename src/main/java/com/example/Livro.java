9c53057ba64670641f3b0759631740a9a89f8051
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-31 15:02:28 +0000

    importando javax

---
+++ src/main/java/com/example/Livro.java
@@ -0,0 +1,50 @@
+
+  @Entity
+  public class Livro implements Serializable {
+	  @Id @GeneratedValue
+	  private Long id;
+	  @Column(nullable=false)
+	  private String titulo;
+	  private String autor;
+	  private Float preco;
+	  @Column(length=2000)
+	  private String descricao;
+	  private Integer numPaginas;
+	  
+	  public Livro() {}
+	  
+	  public Livro(String titulo, String autor, Float preco, String descricao, Integer numPaginas){
+		  this.titulo = titulo;
+		  this.autor = autor;
+		  this.preco = preco;
+		  this.descricao = descricao;
+		  this.numPaginas = numPaginas;
+	  }
+	  
+  	  public void setTitulo(String titulo) {
+		  this.titulo = titulo;
+	  }
+	  public void setAutor(String autor) {
+		  this.autor = autor;
+	  }
+	  public void setPreco(Float preco) {
+		  this.preco = preco;
+	  }
+	  public void setDescricao(String descricao) {
+		  this.descricao = descricao;
+	  }
+	  public void setNumPaginas(Integer numPaginas) {
+		  this.numPaginas = numPaginas;
+	  }
+
+	  public String getTitulo() {
+		  return this.titulo;
+	  }
+	  
+	  @Override
+	  public String toString() {
+		  return "Um livro de " + this.autor;
+	  }
+	  
+  }
+

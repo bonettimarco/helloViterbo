c76e49651a9acb28b146dbe03f2344ee14518486
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-31 16:28:10 +0000

    importando javax

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -79,14 +79,7 @@ public class Main {
     return "index";
   }
 
-
-/*  @RequestMapping("/persiste")
-	  String index() {
-    return "index";
-  }
-	public class Persiste(){
-  @Entity
-	  Livro livro = new Livro("O Guia do Mocheileiro das Galáxias", "Grupo alunos", 40.0F, "Humor e dicção", 380);
+  /*	  Livro livro = new Livro("O Guia do Mocheileiro das Galáxias", "Grupo alunos", 40.0F, "Humor e dicção", 380);
 	  EntityManagerFactory emf = Persistence.createEntityManagerFactory("ExemploJPA");
 	  EntityManager em = emf.createEntityManager();
 	  EntityTransaction et = em.getTransaction();
@@ -95,9 +88,6 @@ public class Main {
 	  et.commit();
 	  em.close();
 	  emf.close();
-
-
-	}
 */
   @RequestMapping("/HelloViterbo")
 			public String olah(@RequestParam("name") String nome, Map<String, Object> model) {

f4efda52a6da58c3541b34bacacbbcfe900ad1e9
Marco Bonetti <marco_bonetti@uol.com.br>
2018-08-31 17:44:19 +0000

    sem Bean

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -17,10 +17,10 @@
 package com.example;
 
 import com.example.Livro;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
